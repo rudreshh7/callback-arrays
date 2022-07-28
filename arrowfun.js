af339c00d09efdb084053795cf3730610f78cef1
Rudresh Jha <74140455+rudreshh7@users.noreply.github.com>
2022-07-28 23:44:56 +0530

    Add files via upload

---
+++ arrowfun.js
@@ -0,0 +1,27 @@
+// Normal Function
+//const add = function(x, y) {
+//  return x * y;
+//}
+
+// Arrow Functions
+const add = (x, y) => {
+    return x / y;
+}
+
+// Arrown Function With String Arguement
+const greet = (name) => {
+    return `Hey ${name}!`;
+}
+
+//Cities and their rating
+//object in Arrays
+
+const Citiesinfo = [
+    { name: 'New Delhi', rating: 4 },
+    { name: 'Hyderabad', rating: 4 },
+    { name: 'Mumbai', rating: 5 },
+    { name: 'Banglore', rating: 3 },
+]
+
+//Maps And For Each
+//maps
